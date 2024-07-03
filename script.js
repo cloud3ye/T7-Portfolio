98d3ebc08044e2f55756b03fcf054da67ad76838
cloud3ye <atted.denz@gmail.com>
2024-07-03 10:48:33 +0100

    fixed code being lfs

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c7c015234216b13f13db44014fe1e12db96460f0c22d1f755a381980e7f66bc
-size 2227
+oid sha256:78582eda1c6a98a0ec1b01d1fea81bf930864efef0c954923b2d2928c8f99cf9
+size 2300
